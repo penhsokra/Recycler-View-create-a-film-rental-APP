44707bf24edf33b195847b0c9f3f9eafe3781f59
penhsokra <penhsokra@gmail.com>
2019-06-24 11:08:07 +0700

    change style

--- app/src/main/java/filmrental/sokra/com/filmrental/DetailActivity.java
+++ app/src/main/java/filmrental/sokra/com/filmrental/DetailActivity.java
@@ -2,6 +2,8 @@ package filmrental.sokra.com.filmrental;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +15,10 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_detail);
         title = findViewById(R.id.txtTitle);
         count = findViewById(R.id.txtCount);
